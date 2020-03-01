25c035a7acf9f843d2baf309be7a9a4e96e6477d
Pos <kazantsevgennadiy@gmail.com>
2020-03-01 18:58:33 +0300

    step four

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
 const actual = readlineSync.question('May I have your name? ');
 const greet = console.log(`Hello, ${actual}!`);
 
-export default greet
+export default greet;
